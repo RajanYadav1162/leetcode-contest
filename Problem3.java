3c66dbf86935450abb38b946fefc542900a4e7c1
rajan <rajanyadav1162@gmail.com>
2024-06-16 10:06:36 +0530

    added solution for leetcode-weekly-402.

--- Problem3.java
+++ Problem3.java
@@ -1,47 +1,43 @@
 
+import java.util.*;
+
 class Problem3 {
 
-  private Integer[][][] memo;
+  public long maximumTotalDamage(int[] power) {
 
-  public int maximumLength(int[] nums, int k) {
+    int n = power.length;
+    long[] dp = new long[n];
+    Arrays.sort(power);
+    long[] suffix = new long[n];
+    suffix[n - 1] = power[n - 1];
+    dp[n - 1] = power[n - 1];
+    for (int i = n - 2; i >= 0; i--) {
+      dp[i] = power[i];
 
-    int n = nums.length;
-    memo = new Integer[n + 1][k + 1][n + 1];
-    return dp(nums, 0, k, n);
+      if (power[i] == power[i + 1]) {
+        dp[i] += dp[i + 1];
+      } else {
+        int idx = find(power, power[i] + 2, i + 1);
+        if (idx != power.length) {
+          dp[i] += suffix[idx];
+        }
+      }
+      suffix[i] = Math.max(dp[i], suffix[i + 1]);
+    }
+
+    return Arrays.stream(dp).max().getAsLong();
   }
 
-  public int dp(int[] A, int i, int k, int prev) {
-
-    if (i == A.length) {
-      return 0;
-    }
-    if (memo[i][k][prev] != null) {
-      return memo[i][k][prev];
-    }
-
-    int c = Integer.MIN_VALUE;
-    int n = A.length;
-    //not take
-    c = Math.max(c, dp(A, i + 1, k, prev));
-
-    //take cases-
-
-    //we can always take the very first item for our subsequence
-    if (prev == n) {
-      c = Math.max(c, dp(A, i + 1, k, i) + 1);
-    } else {
-      //if current item is different from previous item, our k will reduce
-      //eg. if prev and current item is different then we need to have k >0 other wise we can't add it.
-      if (A[prev] != A[i]) {
-        if (k > 0) {
-          c = Math.max(c, dp(A, i + 1, k - 1, i) + 1);
-        }
+  int find(int[] A, int target, int left) {
+    int right = A.length;
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+      if (A[mid] > target) {
+        right = mid;
       } else {
-        //prev and current items are same, we can safely add it to our subsequence.
-        c = Math.max(c, dp(A, i + 1, k, i) + 1);
+        left = mid + 1;
       }
     }
-
-    return memo[i][k][prev] = c;
+    return left;
   }
 }
